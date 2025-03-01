bdb08622085cecf016429b7ea9d4a155bbcd2830
Sarthak Gandotra <iamsarthakgandotra@gmail.com>
2025-03-01 06:39:42 +0000

    modified db conn

--- backend/main.go
+++ backend/main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
+	"backend/db"
 	"fmt"
 	"log"
-	"os"
-
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
-func initDB() {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// Get database connection string from .env
-	dsn := os.Getenv("DATABASE_URL")
-	
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to MySQL:", err)
-	}
-
-	DB = db
-	fmt.Println("Connected to MySQL successfully!")
-}
-
 func main() {
-	initDB()
+	database, err := db.InitDB()
+	if err != nil {
+		log.Fatal("Could not connect to database: ", err)
+	}
+	fmt.Println("Database connected: ", database)
+
 }
