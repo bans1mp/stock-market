a4718b2796131ba61c405fd61a3f469b09cea160
Sarthak Gandotra <iamsarthakgandotra@gmail.com>
2025-03-01 20:23:11 +0000

    Added GRPC communication between cpp and golang code

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,15 @@ import (
 	"backend/controller"
 	"backend/db"
 	"backend/middleware"
+	trade "backend/proto"
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -16,6 +21,32 @@ func main() {
 		log.Fatal("Could not connect to database: ", err)
 	}
 	fmt.Println("Database connected: ", database)
+
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Error creating GRPC connection to server")
+	}
+	defer conn.Close()
+
+	client := trade.NewMatchingEngineClient(conn)
+
+	req := &trade.OrderRequest{
+		UserId: 1,
+		Symbol: "AAPL",
+		Price: 200,
+		Quantity: 10,
+		OrderType: "BUY",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.PlaceOrder(ctx, req)
+	if err != nil {
+		log.Fatalf("Error calling PlaceOrder: %v", err)
+	}
+
+	fmt.Println("Server Response:", res)
 
 	r := gin.Default()
 
