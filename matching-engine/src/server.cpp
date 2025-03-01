b7eacf1fa0ae2029eb0ec9c6d79a86481fd28a58
Sarthak Gandotra <iamsarthakgandotra@gmail.com>
2025-03-01 13:23:40 +0000

    Added c++ grpc server to listen to requests

---
+++ matching-engine/src/server.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "../proto/trade.grpc.pb.h"  // Include gRPC generated headers
+
+using namespace std ;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using trade::OrderRequest;
+using trade::OrderResponse;
+using trade::MatchingEngine;
+
+// Implement the MatchingEngine gRPC service
+class MatchingEngineImpl final : public MatchingEngine::Service {
+public:
+    Status PlaceOrder(ServerContext* context, const OrderRequest* request, OrderResponse* response) override {
+        cout << "Received order from user: " << request->user_id() << endl;
+        cout << "Order type: " << request->order_type() << ", Quantity: " << request->quantity() << endl;
+
+        // process order here
+
+        response->set_message("Order Placed");
+        response->set_success(true);
+
+        cout << "Order processed\n";
+        return Status::OK;
+    }
+};
+
+// Start the gRPC server
+void RunServer() {
+    string server_address("0.0.0.0:50051");
+    MatchingEngineImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    unique_ptr<Server> server(builder.BuildAndStart());
+
+    cout << "Matching Engine Server listening on " << server_address << endl;
+    server->Wait();
+}
+
+int main() {
+    RunServer();
+    return 0;
+}
