9a9a0fdf54f309752a112d5bf599e2b5b3bb7f3c
Sarthak Gandotra <iamsarthakgandotra@gmail.com>
2025-03-03 17:58:57 +0000

    fixed redis error

--- matching-engine/src/redis.cpp
+++ matching-engine/src/redis.cpp
@@ -3,31 +3,25 @@
 
 using namespace std;
 
-class RedisClient {
-private:
-    redisContext* conn ;
-
-    RedisClient(){
-        conn = redisConnect("127.0.0.1", 6379);
-        if (conn == NULL || conn->err) {
-            cerr << "Connection error: " << (conn ? conn->errstr : "NULL context") << std::endl;
-            conn = nullptr;
-        }
+RedisClient::RedisClient() {
+    conn = redisConnect("127.0.0.1", 6379);
+    if (conn == NULL || conn->err) {
+        cerr << "Connection error: " << (conn ? conn->errstr : "NULL context") << endl;
+        conn = nullptr;
     }
+}
 
-public:
-    static RedisClient& getInstance(){
-        static RedisClient instance ;
-        return instance;
-    }
+RedisClient& RedisClient::getInstance() {
+    static RedisClient instance;
+    return instance;
+}
 
-    redisContext* getConnection() {
-        return conn;
-    }
+redisContext* RedisClient::getConnection() {
+    return conn;
+}
 
-    ~RedisClient() {
-        if(conn) {
-            redisFree(conn);
-        }
+RedisClient::~RedisClient() {
+    if (conn) {
+        redisFree(conn);
     }
-};
+}
