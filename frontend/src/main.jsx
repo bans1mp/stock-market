d1b928d5f8affe9b643ea52a7a957cd21db3ec66
Sarthak Gandotra <iamsarthakgandotra@gmail.com>
2025-03-13 20:25:44 +0000

    Added home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Market from "./pages/Market";
 import Orders from "./pages/Orders";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
